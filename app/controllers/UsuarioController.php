7ca413433f8edeac10855dfa2ebdfdb0b1f4d512
Cata Allende <franallende2000@gmail.com>
2022-11-09 15:22:32 -0300

    proyecto funcionando con funciones mias

---
+++ app/controllers/UsuarioController.php
@@ -0,0 +1,97 @@
+<?php
+require_once './models/Usuario.php';
+require_once './interfaces/IApiUsable.php';
+
+class UsuarioController extends Usuario implements IApiUsable
+{
+    public function CargarUno($request, $response, $args)
+    {
+        $parametros = $request->getParsedBody();
+
+        $usuario = $parametros['usuario'];
+        $clave = $parametros['clave'];
+
+        // Creamos el usuario
+        $usr = new Usuario();
+        $usr->usuario = $usuario;
+        $usr->clave = $clave;
+        $usr->crearUsuario();
+
+        $payload = json_encode(array("mensaje" => "Usuario creado con exito"));
+
+        $response->getBody()->write($payload);
+        return $response
+          ->withHeader('Content-Type', 'application/json');
+    }
+
+    public function TraerUno($request, $response, $args)
+    {
+        // Buscamos usuario por nombre
+        $usr = $args['usuario'];
+        $usuario = Usuario::obtenerUsuario($usr);
+        $payload = json_encode($usuario);
+
+        $response->getBody()->write($payload);
+        return $response
+          ->withHeader('Content-Type', 'application/json');
+    }
+
+    public function TraerTodos($request, $response, $args)
+    {
+        $lista = Usuario::obtenerTodos();
+        $payload = json_encode(array("listaUsuario" => $lista));
+
+        $response->getBody()->write($payload);
+        return $response
+          ->withHeader('Content-Type', 'application/json');
+    }
+    
+    public function ModificarClavePorNombre($request, $response, $args)
+    {
+        $parametros = $request->getParsedBody();
+
+        $usr = $args['usuario'];
+        $u = Usuario::getIDByName($usr);
+
+        $claveACambiar = $parametros['clave'];
+
+        Usuario::modificarClaveUsuarioPorNombre($u->id, $claveACambiar);
+
+        $payload = json_encode(array("mensaje" => "Clave modificada con exito"));
+
+        $response->getBody()->write($payload);
+        return $response
+          ->withHeader('Content-Type', 'application/json');
+    }
+
+    public function ModificarUno($request, $response, $args)
+    {
+        $parametros = $request->getParsedBody();
+
+        $id = $parametros['id'];
+        $usuario = $parametros['usuario'];
+        $clave = $parametros['clave'];
+
+        Usuario::modificarUsuario($id, $usuario, $clave);
+
+        $payload = json_encode(array("mensaje" => "Usuario modificado con exito"));
+
+        $response->getBody()->write($payload);
+        return $response
+          ->withHeader('Content-Type', 'application/json');
+    }
+
+    public function BorrarUno($request, $response, $args)
+    {
+        $parametros = $request->getParsedBody();
+
+        $usuarioId = $parametros['id'];
+        Usuario::borrarUsuario($usuarioId);
+
+        $payload = json_encode(array("mensaje" => "Usuario borrado con exito"));
+
+        $response->getBody()->write($payload);
+        return $response
+          ->withHeader('Content-Type', 'application/json');
+    }
+}
---
+++ app/index.php
@@ -0,0 +1,56 @@
+<?php
+// Error Handling
+error_reporting(-1);
+ini_set('display_errors', 1);
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Factory\AppFactory;
+use Slim\Routing\RouteCollectorProxy;
+use Slim\Routing\RouteContext;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+require_once './db/AccesoDatos.php';
+// require_once './middlewares/Logger.php';
+
+require_once './controllers/UsuarioController.php';
+
+// Load ENV
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+// Instantiate App
+$app = AppFactory::create();
+
+// Set base path
+$app->setBasePath('/repo_heroku/slim-php-mysql-heroku/app');
+
+// Add error middleware
+$app->addErrorMiddleware(true, true, true);
+
+// Add parse body
+$app->addBodyParsingMiddleware();
+
+// Routes
+$app->group('/usuarios', function (RouteCollectorProxy $group) {
+    $group->get('[/]', \UsuarioController::class . ':TraerTodos'); //corchete opcional
+    $group->get('/{usuario}', \UsuarioController::class . ':TraerUno'); //llaves es variable
+    $group->post('[/]', \UsuarioController::class . ':CargarUno');
+    $group->put('[/]', \UsuarioController::class . ':ModificarUno');
+    $group->put('/{usuario}', \UsuarioController::class . ':ModificarClavePorNombre');
+    $group->delete('[/]', \UsuarioController::class . ':BorrarUno');
+  });
+
+
+
+$app->get('[/]', function (Request $request, Response $response) {    
+    $response->getBody()->write("Slim Framework 4 PHP Francisco Allende");
+    return $response;
+
+});
+
+$app->run();
+
+
